d47d105d0ec5d3ab6d004c9ffce0fc5f0a0a966d
XxYassinexX <yassinesoltani19981998@gmail.com>
2022-02-28 08:15:15 +0100

    first commit

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import userRoutes from "./routes/user.js";
 import routerPosts from "./routes/posts.js";
 const app = express();
 dotenv.config();
+import path from "path";
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -16,6 +17,18 @@ app.use(cors());
 app.use("/api/car", carRouter);
 app.use("/api/user", userRoutes);
 app.use("/api/posts", routerPosts);
+
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../Client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "Client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 Mongoose.connect(process.env.MONGOURL);
 app.listen(process.env.PORT || 5000, () => {
